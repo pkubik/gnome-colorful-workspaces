81773adeae9def2ebb39b7ad09e0f1f0f8a7b840
Pawel Kubik <p.kubik.pl@gmail.com>
2019-09-28 22:16:05 +0200

    Implement colorful indicator with

--- extension.js
+++ extension.js
@@ -5,61 +5,39 @@ const Tweener = imports.ui.tweener;
 
 let text, button;
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
-
-function _onWorkspaceSwitched() {
-    // TODO: Actually use...
-    this._currentWorkspace = global.workspace_manager.get_active_workspace_index();
-
-    this._updateMenuOrnament();
-    this._updateActiveThumbnail();
-
-    this._statusLabel.set_text(this._labelText());
-}
-
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
-    }
-
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-                      monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
-}
-
 class Extension {
     constructor() {
-        this.button = new St.Bin({ style_class: 'panel-button',
-        reactive: true,
-        can_focus: true,
-        x_fill: true,
-        y_fill: true,
-        track_hover: true });
-        let label = new St.Label({ text: '  w  ', style_class: 'workspace-indicator' });
+        this._button = new St.Bin({ style_class: 'panel-button',
+            reactive: true,
+            can_focus: true,
+            x_fill: true,
+            y_fill: true,
+            track_hover: true
+        });
+        this._label = new St.Label({ text: '  w  ', style_class: 'workspace-indicator-1' });
+        this._button.set_child(this._label);
+    }
 
-        this.button.set_child(label);
-        this.button.connect('button-press-event', _showHello);
+    _onWorkspaceSwitched() {
+        let workspaceIndex = global.workspace_manager.get_active_workspace_index();
+        this._label.set_text("   " + (workspaceIndex + 1) + "   ");
+        this._label.style_class = 'workspace-indicator-' + (workspaceIndex % 4 + 1);
+        Main.panel.style_class = 'upper-glow-' + (workspaceIndex % 4 + 1);
     }
 
     enable() {
-        Main.panel._rightBox.insert_child_at_index(this.button, 0);
+        Main.panel._rightBox.insert_child_at_index(this._button, 0);
+        Main.panel.style_class = 'upper-glow-1';
+
+        this._workspaceSwitchedSignal = 
+            global.workspace_manager.connect_after('workspace-switched',
+                this._onWorkspaceSwitched.bind(this));
     }
 
     disable() {
-        Main.panel._rightBox.remove_child(this.button);
+        Main.panel._rightBox.remove_child(this._button);
+        Main.panel.style_class = null;
+        global.workspace_manager.disconnect(this._workspaceSwitchedSignal);
     }
 }
 
